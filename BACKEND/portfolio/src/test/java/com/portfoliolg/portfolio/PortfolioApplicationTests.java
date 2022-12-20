58fc5d5fee889e8a9bda68e31346b8ae8d1ac26c
Luci4P1r1 <97774175+Luci4P1r1@users.noreply.github.com>
2022-12-19 23:07:39 -0300

    SE AÑADE BACK

---
+++ BACKEND/portfolio/src/test/java/com/portfoliolg/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliolg.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
